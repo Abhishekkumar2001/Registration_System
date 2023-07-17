08fa90193d9c98d813afb1273e7b5d64365f2bb4
ABHISHEK KUMAR <72004395+Abhishekkumar2001@users.noreply.github.com>
2023-07-18 00:05:30 +0530

    Created the Schema

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const mongoose = require('mongoose');
 const app = express();
 
 
-// Connected to the database
+// Connecting to the database
 mongoose.connect("mongodb://127.0.0.1:27017/signupDB").then(() =>{
     console.log("App is now connected to DB");
 }).catch((err) =>{
     console.log(`${err}`);
 })
 
-// Listening the server
+// Listening to app
 app.listen(port, (err) =>{
     if(err){
         console.log(`${err}`);
---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+//Creating the Schema
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true
+    }
+}, {timestamps : true});
+
+// To import the Schema anywhere in the project
+
+module.exports = mongoose.model("User", userSchema);
